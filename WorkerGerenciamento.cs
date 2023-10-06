9507e28432ea0bff91ddf2fd732a9894ab810d56
Elton Alves <elton@adxspace.com.br>
2023-10-06 14:51:37 -0300

    Correcao do consumo da mensageria

--- WorkerGerenciamento.cs
+++ WorkerGerenciamento.cs
@@ -14,34 +14,35 @@ namespace chwhatsappgpt;
 public static class WorkerGerenciamento
 {
     [FunctionName("WorkerGerenciamento")]
-    public static void Run(
+    public static async Task RunAsync(
         [TimerTrigger("0 0/1 * * * *")] TimerInfo myTimer,
         ILogger log)
     {
         log.LogInformation(Environment.GetEnvironmentVariable("URL_GERENCIAMENTO"));
         log.LogInformation("Início do worker");
-        LerFila(log);        
+        await LerFila(log);        
         log.LogInformation("Final fila.....");
     }
 
-    private static void LerFila(ILogger log)
+    private async static Task LerFila(ILogger log)
     {
         var factory = new ConnectionFactory { Uri = new Uri(Environment.GetEnvironmentVariable("URL_GERENCIAMENTO")) };
 
         using var connection = factory.CreateConnection();
         using var channel = connection.CreateModel();
 
-        channel.QueueDeclare(queue: "gerenciamento",
-                             durable: true,
-                             exclusive: false,
-                             autoDelete: false,
-                             arguments: null);
-
-        log.LogInformation(" [*] Waiting for messages.");
+        channel.QueueDeclare(
+                            queue: "gerenciamento",
+                            durable: true,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
 
         var consumer = new EventingBasicConsumer(channel);
+
         consumer.Received += async (model, ea) =>
         {
+            log.LogInformation("Tem Msg");
             var body = ea.Body.ToArray();
             var message = Encoding.UTF8.GetString(body);
             var msg = JsonConvert.DeserializeObject<Gerenciamento>(message);
@@ -52,13 +53,11 @@ public static class WorkerGerenciamento
                 channel.BasicAck(ea.DeliveryTag, false);
             else
                 channel.BasicNack(ea.DeliveryTag, false, false);
-
-            log.LogInformation($" [x] Received {message}");
         };
 
-        channel.BasicConsume(queue: "gerenciamento",
-                             autoAck: false,
-                             consumer: consumer);
+        channel.BasicConsume("gerenciamento",
+                             false,
+                             consumer);
     }
 
     private async static Task<bool> Enviar(Gerenciamento gerenciamento, ILogger log)
